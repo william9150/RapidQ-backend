456c7740e7a104f0cb26b5a2427ad6a51fd2546c
ColdingPoTaTo <william.9150@gmail.com>
2023-09-20 18:01:26 +0800

    [feat] add repoModel

---
+++ models/repoModel.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const optionSchema = new mongoose.Schema(
+    {
+        idx: {
+            type: Number,
+        },
+        desc: {
+            type: String,
+        },
+        imgUrl: {
+            type: String,
+        },
+    },
+    { _id: false },
+); // 設定不生成_id
+
+const questionSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['Single', 'Multi'],
+    },
+    title: {
+        type: String,
+    },
+    imgUrl: {
+        type: String,
+    },
+    options: [optionSchema],
+    answer: {
+        type: [Number],
+    },
+});
+
+const repoSchema = new mongoose.Schema({
+    repoName: {
+        type: String,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    questions: [questionSchema],
+    isDelete: {
+        type: Boolean,
+        default: false,
+    },
+    isPublic: {
+        type: Boolean,
+        default: true,
+    },
+    createAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+const Repo = mongoose.model('Repository', repoSchema);
+
+module.exports = Repo;
